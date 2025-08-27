952e32c56e25b1a9469030d996ccbcdcfa2aac8e
Halil Yildirim <ce.hybusiness@gmail.com>
2025-08-27 03:02:52 +0300

    feat: Add dynamic Service section
    This commit introduces the necessary backend and frontend features to dynamically manage and display services. The static content has been replaced with data fetched from the database, enhancing flexibility and maintainability.
    
    - **Backend:** Created the `ServicesController` to handle API requests for the Service entity.
    - **Frontend:** Revitalized the `Index.cshtml` page to consume data dynamically from the newly implemented Service API.
    - **Views:** Added the `Views/Service/Index.cshtml` page to serve as the main view for displaying service information.
    - **Data Flow:** Ensured a seamless data flow, allowing service data to be fetched from the database and rendered on the frontend.

---
+++ Frontends/DriveNow.WebUI/Controllers/ServiceController.cs
@@ -0,0 +1,25 @@
+﻿using DriveNow.Dtos.ServiceDtos;
+using DriveNow.Dtos.TestimonialDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace DriveNow.WebUI.Controllers
+{
+    public class ServiceController(IHttpClientFactory _httpClientFactory) : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7031/api/Services");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
+                return View(values);
+            }
+
+            return View();
+
+        }
+    }
+}
