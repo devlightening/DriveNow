a730569694d75993374666bd3c896b5278336b02
Halil Yildirim <ce.hybusiness@gmail.com>
2025-08-27 15:21:50 +0300

    feat(Contact): Add Contact Page with Dynamic Footer Address
    - **feat(WebUI/Controllers):** Create a new `ContactController` to handle the contact page logic.
    - **feat(WebUI/Views):** Add `Index.cshtml` view for the contact page and a `_FooterAddressCompenentPartial` partial view to display footer details.
    - **feat(WebUI/DTOs):** Introduce `ResultFooterAddressDto` to map footer address data.
    - **feat(WebUI/ViewComponents):** Implement `_FooterAddressCompenentPartial.cs` view component to fetch and pass footer address data to the partial view.
    - **feat(WebUI/Layout):** Modify `_Layout.cshtml` to include the new footer address view component, ensuring it's displayed on all pages.
    - **feat(WebUI/Styling):** Update `_ViewImports.cshtml` to include necessary component and model imports.

---
+++ Frontends/DriveNow.WebUI/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DriveNow.WebUI.Controllers
+{
+    public class ContactController(IHttpClientFactory _httpClientFactory) : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
